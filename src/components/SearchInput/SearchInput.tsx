58e13de7431915aa959040e2ad741d5886f3099b
Usman Butt <usman.butt@outlook.com>
2024-01-03 05:56:54 -0500

    Refactor SearchInput component to remove unused state and lifted the stated

--- src/components/SearchInput/SearchInput.tsx
+++ src/components/SearchInput/SearchInput.tsx
@@ -1,29 +1,37 @@
-import { useId, useState } from 'react';
+import { useId } from 'react';
 
-function SearchInput() {
-  const id = useId();
-  const [search, setSearch] = useState('');
+type SearchInputProps = {
+  setSearch: (search: string) => void;
+};
+
+function SearchInput({ setSearch }: SearchInputProps) {
+  const Id = useId();
 
   function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
     setSearch(e.target.value);
-    console.log(search);
   }
 
   return (
-    <div className='relative flex items-center mt-2'>
+    <form className='flex items-center mt-2'>
+      <label htmlFor={`search-input-${Id}`} className='sr-only'>
+        Search
+      </label>
       <input
+        id={`search-input-${Id}`}
         type='text'
         name='search'
-        id={`search-${id}`}
-        value={search}
+        // value={search}
         onChange={handleChange}
         placeholder='Search'
         className='block w-full rounded-md border-0 py-1.5 pr-14 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
       />
-      <div className='absolute inset-y-0 right-0 flex items-center pr-3 pointer-events-none'>
-        <img src='icon-search.svg' alt='Search' />
-      </div>
-    </div>
+
+      <img
+        src='icon-search.svg'
+        alt='Search'
+        className='inset-y-0 right-0 flex items-center pr-3 pointer-events-none'
+      />
+    </form>
   );
 }
 
