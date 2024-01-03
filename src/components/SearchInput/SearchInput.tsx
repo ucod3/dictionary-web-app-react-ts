ef6f62bacd2fd22d6ae3d38f257a304a23202d36
Usman Butt <usman.butt@outlook.com>
2024-01-03 07:59:35 -0500

    Add inputWord prop to SearchInput component

--- src/components/SearchInput/SearchInput.tsx
+++ src/components/SearchInput/SearchInput.tsx
@@ -1,11 +1,16 @@
 import { useId } from 'react';
 
 type SearchInputProps = {
+  inputWord: string;
   setInputWord: (search: string) => void;
   handleSubmit: (e: React.FormEvent<HTMLFormElement>) => void;
 };
 
-function SearchInput({ setInputWord, handleSubmit }: SearchInputProps) {
+function SearchInput({
+  inputWord,
+  setInputWord,
+  handleSubmit,
+}: SearchInputProps) {
   const Id = useId();
 
   function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
@@ -21,6 +26,7 @@ function SearchInput({ setInputWord, handleSubmit }: SearchInputProps) {
         id={`search-input-${Id}`}
         type='text'
         name='search'
+        value={inputWord}
         onChange={handleChange}
         placeholder='Search'
         className='block w-full rounded-md border-0 py-1.5 pr-14 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
