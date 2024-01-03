6b6fb926c71b5798c148975026e29909b2381f62
Usman Butt <usman.butt@outlook.com>
2024-01-03 06:05:04 -0500

    Remove commented out code in SearchInput component

--- src/components/SearchInput/SearchInput.tsx
+++ src/components/SearchInput/SearchInput.tsx
@@ -20,7 +20,6 @@ function SearchInput({ setSearch }: SearchInputProps) {
         id={`search-input-${Id}`}
         type='text'
         name='search'
-        // value={search}
         onChange={handleChange}
         placeholder='Search'
         className='block w-full rounded-md border-0 py-1.5 pr-14 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
