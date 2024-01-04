448e91f8f5765486a8c04a650610cfca4febf2c3
Usman Butt <usman.butt@outlook.com>
2024-01-04 17:49:20 -0500

    Refactor Header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,9 +2,7 @@ import FontSwitcher from '../FontSwitcher';
 import Logo from '../Logo';
 import ThemeSwitcher from '../ThemeSwitcher';
 
-type HeaderProps = {};
-
-function Header(props: HeaderProps) {
+function Header() {
   return (
     <header>
       <Logo />
