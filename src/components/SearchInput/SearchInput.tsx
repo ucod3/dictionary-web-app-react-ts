e9db8df687e2d0c876043057df565fc7fad6d55f
Usman Butt <usman.butt@outlook.com>
2024-01-03 21:46:51 -0500

    Refactor SearchInput component to improve accessibility and styling

--- src/components/SearchInput/SearchInput.tsx
+++ src/components/SearchInput/SearchInput.tsx
@@ -18,7 +18,10 @@ function SearchInput({
   }
 
   return (
-    <form className='flex items-center mt-2' onSubmit={handleSubmit}>
+    <form
+      className='relative flex items-center justify-between mt-2'
+      onSubmit={handleSubmit}
+    >
       <label htmlFor={`search-input-${Id}`} className='sr-only'>
         Search
       </label>
@@ -28,15 +31,18 @@ function SearchInput({
         name='search'
         value={inputWord}
         onChange={handleChange}
-        placeholder='Search'
+        placeholder='Search for any word…'
         className='block w-full rounded-md border-0 py-1.5 pr-14 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
       />
 
       <img
         src='icon-search.svg'
         alt='Search'
-        className='inset-y-0 right-0 flex items-center pr-3 pointer-events-none'
+        className='absolute right-0 pr-3 pointer-events-none inset-y-2 '
       />
+      {/* <svg className='absolute right-0 pr-3 bg-blue-500 pointer-events-none inset-y-2 '>
+        <use xlinkHref='icon-search.svg' />
+      </svg> */}
     </form>
   );
 }
