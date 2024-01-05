7f99731548db8387f16a83002ee53e3b1aa2fa8b
Usman Butt <usman.butt@outlook.com>
2024-01-05 10:20:13 -0500

    Update Tailwind config with dark mode and new color variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  darkMode: 'class',
   theme: {
     colors: {
-      'gray-darkest': 'hsl(var(--color-gray-darkest) / var(--tw-bg-opacity))',
-      'gray-darker': 'hsl(var(--color-gray-darker) / var(--tw-bg-opacity))',
-      'gray-dark': 'hsl(var(--color-gray-dark) / var(--tw-bg-opacity))',
-      gray: 'hsl(var(--color-gray) / var(--tw-bg-opacity))',
-      'gray-light': 'hsl(var(--color-gray-light) / var(--tw-bg-opacity))',
-      'gray-lighter': 'hsl(var(--color-gray-lighter) / var(--tw-bg-opacity))',
-      'gray-lightest': 'hsl(var(--color-gray-lightest) / var(--tw-bg-opacity))',
-      white: 'hsl(var(--color-white) / var(--tw-bg-opacity))',
-      purple: 'hsl(var(--color-purple) / var(--tw-bg-opacity))',
-      red: 'hsl(var(--color-red) / var(--tw-bg-opacity))',
+      textColor: {
+        'gray-dark': 'hsl(var(--color-gray-dark) / <alpha-value>)',
+      },
+      'gray-darkest': 'hsl(var(--color-gray-darkest) / <alpha-value>)',
+      'gray-darker': 'hsl(var(--color-gray-darker) / <alpha-value>)',
+      gray: 'hsl(var(--color-gray) / <alpha-value>)',
+      'gray-light': 'hsl(var(--color-gray-light) / <alpha-value>)',
+      'gray-lighter': 'hsl(var(--color-gray-lighter) / <alpha-value>)',
+      'gray-lightest': 'hsl(var(--color-gray-lightest) / <alpha-value>)',
+      white: 'hsl(var(--color-white) / <alpha-value>)',
+      purple: 'hsl(var(--color-purple) / <alpha-value>)',
+      red: 'hsl(var(--color-red) / <alpha-value>)',
     },
     fontFamily: {
       sans: ['Inter', 'sans-serif'],
@@ -26,6 +29,8 @@ module.exports = {
       'body-m': ['18px', '24px'],
       'body-s': ['14px', '17px'],
     },
+    extend: {},
   },
+
   plugins: [require('@tailwindcss/forms')],
 };
