1ee84065da9bb8589c8365f671b95b165d76828e
Usman Butt <usman.butt@outlook.com>
2024-01-06 08:31:33 -0500

    Refactor useTheme hook to use setAndStoreTheme

--- src/hooks/useTheme.tsx
+++ src/hooks/useTheme.tsx
@@ -6,6 +6,6 @@ export default function useTheme() {
   if (!context) {
     throw new Error('useTheme must be used within a ThemeProvider');
   }
-  const { theme, setTheme } = context;
-  return { theme, setTheme };
+  const { theme, setAndStoreTheme } = context;
+  return { theme, setTheme: setAndStoreTheme };
 }
