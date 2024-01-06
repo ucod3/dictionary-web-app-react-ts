c19c55d077ed0fe020fb491f59a08cddcebf5a44
Usman Butt <usman.butt@outlook.com>
2024-01-06 10:45:26 -0500

    Refactor ThemeSwitcher component

--- src/components/ThemeSwitcher/ThemeSwitcher.tsx
+++ src/components/ThemeSwitcher/ThemeSwitcher.tsx
@@ -13,11 +13,12 @@ function ThemeSwitcher() {
   };
 
   return (
-    <>
+    <div className='flex items-center gap-0'>
       <Switch
         checked={theme === 'dark'}
+        className='border-style-none'
+        color='dark/white'
         onChange={handleThemeChange}
-        className='w-10 h-10 bg-gray-200 rounded-full focus:outline-none'
       />
 
       <svg
@@ -27,7 +28,7 @@ function ThemeSwitcher() {
         viewBox='0 0 81 22'
         fill='none'
         strokeWidth={1.5}
-        className='transition-colors duration-200 ease-in-out stroke-toggle-accent hover:stroke-primary-accent'
+        className='p-0 transition-colors duration-200 ease-in-out stroke-primary-accent hover:stroke-primary-accent ml-[-36px]'
       >
         <path
           strokeLinecap='round'
@@ -35,7 +36,7 @@ function ThemeSwitcher() {
           d='M60 10.449C59.9985 12.8283 60.8017 15.1383 62.2791 17.0033C63.7566 18.8683 65.8214 20.1788 68.138 20.7218C70.4545 21.2647 72.8866 21.0082 75.039 19.994C77.1912 18.9797 78.9373 17.2673 79.9931 15.1352C70.5442 15.1352 65.858 10.4479 65.858 1C64.0984 1.87311 62.6177 3.22033 61.5827 4.88981C60.5476 6.5593 59.9995 8.48469 60 10.449Z'
         />
       </svg>
-    </>
+    </div>
   );
 }
 
