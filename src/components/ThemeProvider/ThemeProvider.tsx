002b5a69ccab824110acd78af5c3ad6323c11fed
Usman Butt <usman.butt@outlook.com>
2024-01-05 23:25:24 -0500

    Add ThemeProvider component

---
+++ src/components/ThemeProvider/ThemeProvider.tsx
@@ -0,0 +1,76 @@
+import { createContext, useEffect, useMemo, useState } from 'react';
+
+type Theme = 'dark' | 'light' | 'system';
+
+type ThemeProviderProps = {
+  children: React.ReactNode;
+  defaultTheme?: Theme;
+  storageKey?: string;
+};
+
+type ThemeProviderState = {
+  theme: Theme;
+  setTheme: (theme: Theme) => void;
+};
+
+const initialState: ThemeProviderState = {
+  theme: 'system',
+  setTheme: () => {},
+};
+
+const ThemeContext = createContext<ThemeProviderState>(initialState);
+function ThemeProvider({
+  children,
+ defaultTheme = 'system',
+storageKey = 'theme',
+}: ThemeProviderProps) {
+  const [theme, setTheme] = useState<Theme>(() => {
+    const storedTheme = localStorage.getItem(storageKey);
+    if (storedTheme) {
+      return storedTheme;
+    }
+    if (defaultTheme === 'system') {
+      return window.matchMedia('(prefers-color-scheme: dark)').matches
+        ? 'dark'
+        : 'light';
+    }
+    return defaultTheme;
+  });
+
+  const setAndStoreTheme = (newTheme: Theme) => {
+    const root = window.document.documentElement;
+    let finalTheme = newTheme;
+
+    if (newTheme === 'system') {
+      finalTheme = window.matchMedia('(prefers-color-scheme: dark)').matches
+        ? 'dark'
+        : 'light';
+    }
+
+    root.setAttribute('data-mode', finalTheme);
+    localStorage.setItem(storageKey, finalTheme);
+    setTheme(finalTheme);
+  };
+
+  useEffect(() => {
+    setAndStoreTheme(theme);
+  }, [theme]);
+}
+
+  const value = useMemo(
+    () => ({
+      theme,
+      setTheme: (newTheme: Theme) => {
+        localStorage.setItem(storageKey, newTheme);
+        setTheme(newTheme);
+      },
+    }),
+    [theme, storageKey],
+  );
+
+  return (
+    <ThemeContext.Provider value={value}>{children}</ThemeContext.Provider>
+  );
+}
+
+export default ThemeProvider;
