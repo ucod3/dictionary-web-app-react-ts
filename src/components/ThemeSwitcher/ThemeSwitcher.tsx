ec69ce0128f6d6c05fe212c7ba881e246eb0822d
Usman Butt <usman.butt@outlook.com>
2024-01-06 08:31:39 -0500

    Add theme switcher component

--- src/components/ThemeSwitcher/ThemeSwitcher.tsx
+++ src/components/ThemeSwitcher/ThemeSwitcher.tsx
@@ -1,20 +1,47 @@
+import useTheme from '../../hooks/useTheme';
+
+type Theme = 'light' | 'dark';
+
 function ThemeSwitcher() {
+  const { theme, setTheme } = useTheme();
+
+  const handleThemeChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const newTheme = e.target.value as Theme;
+    setTheme(newTheme, true);
+  };
+
   return (
-    <svg
-      xmlns='http://www.w3.org/2000/svg'
-      width='81'
-      height='22'
-      viewBox='0 0 81 22'
-      fill='none'
-      strokeWidth={1.5}
-      className='transition-colors duration-200 ease-in-out stroke-toggle-accent hover:stroke-primary-accent'
-    >
-      <path
-        strokeLinecap='round'
-        strokeLinejoin='round'
-        d='M60 10.449C59.9985 12.8283 60.8017 15.1383 62.2791 17.0033C63.7566 18.8683 65.8214 20.1788 68.138 20.7218C70.4545 21.2647 72.8866 21.0082 75.039 19.994C77.1912 18.9797 78.9373 17.2673 79.9931 15.1352C70.5442 15.1352 65.858 10.4479 65.858 1C64.0984 1.87311 62.6177 3.22033 61.5827 4.88981C60.5476 6.5593 59.9995 8.48469 60 10.449Z'
-      />
-    </svg>
+    <>
+      {/* create a toggle switch */}
+      <label htmlFor='theme-switcher' className='sr-only'>
+        Theme
+      </label>
+      <select
+        id='theme-switcher'
+        onChange={handleThemeChange}
+        value={theme}
+        className='w-10 h-10 bg-gray-200 rounded-full focus:outline-none'
+      >
+        <option value='light'>Light</option>
+        <option value='dark'>Dark</option>
+      </select>
+
+      <svg
+        xmlns='http://www.w3.org/2000/svg'
+        width='81'
+        height='22'
+        viewBox='0 0 81 22'
+        fill='none'
+        strokeWidth={1.5}
+        className='transition-colors duration-200 ease-in-out stroke-toggle-accent hover:stroke-primary-accent'
+      >
+        <path
+          strokeLinecap='round'
+          strokeLinejoin='round'
+          d='M60 10.449C59.9985 12.8283 60.8017 15.1383 62.2791 17.0033C63.7566 18.8683 65.8214 20.1788 68.138 20.7218C70.4545 21.2647 72.8866 21.0082 75.039 19.994C77.1912 18.9797 78.9373 17.2673 79.9931 15.1352C70.5442 15.1352 65.858 10.4479 65.858 1C64.0984 1.87311 62.6177 3.22033 61.5827 4.88981C60.5476 6.5593 59.9995 8.48469 60 10.449Z'
+        />
+      </svg>
+    </>
   );
 }
 
