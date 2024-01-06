d1438cc90135bf02ddb8edef1e9037509b714026
Usman Butt <usman.butt@outlook.com>
2024-01-06 09:23:28 -0500

    Refactor useTheme hook

--- src/hooks/useTheme.tsx
+++ src/hooks/useTheme.tsx
@@ -1,11 +1,15 @@
 import { useContext } from 'react';
 import { ThemeContext } from '../components/ThemeProvider/ThemeProvider';
+// import { Theme } from '../types/themeTypes';
 
 export default function useTheme() {
   const context = useContext(ThemeContext);
   if (!context) {
     throw new Error('useTheme must be used within a ThemeProvider');
   }
-  const { theme, setAndStoreTheme } = context;
-  return { theme, setTheme: setAndStoreTheme };
+  const { theme, setAndStoreTheme } = useContext(ThemeContext);
+  return {
+    theme,
+    setAndStoreTheme,
+  };
 }
