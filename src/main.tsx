d099e9867bf04dcaaafa2328be438ce5b6f494c9
Usman Butt <usman.butt@outlook.com>
2024-01-07 18:50:10 -0500

    Add ThemeProvider to App component

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App';
 import './index.css';
+import ThemeProvider from './components/ThemeProvider';
 
 ReactDOM.createRoot(document.querySelector('#root')!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
   </StrictMode>,
 );
