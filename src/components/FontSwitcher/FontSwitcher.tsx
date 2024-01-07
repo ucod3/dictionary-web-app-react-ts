79d22ae3be70337b0fa9a657b27ba6b669f0c675
Usman Butt <usman.butt@outlook.com>
2024-01-07 15:26:59 -0500

    Refactor FontSwitcher component to include a select dropdown for font selection

--- src/components/FontSwitcher/FontSwitcher.tsx
+++ src/components/FontSwitcher/FontSwitcher.tsx
@@ -1,7 +1,21 @@
-type FontSwitcherProps = {};
-
-function FontSwitcher(props: FontSwitcherProps) {
-  return null;
+function FontSwitcher() {
+  return (
+    <div>
+      <label htmlFor='location' className='sr-only'>
+        Location
+      </label>
+      <select
+        id='location'
+        name='location'
+        className='border-0 text-primary-foreground-700 bg-primary'
+        defaultValue='Sans Serif'
+      >
+        <option>Sans Serif</option>
+        <option>Serif</option>
+        <option>Mono</option>
+      </select>
+    </div>
+  );
 }
 
 export default FontSwitcher;
