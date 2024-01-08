7836d5a2ca7bd8fa68ff240a70746193b194fe2e
Usman Butt <usman.butt@outlook.com>
2024-01-07 23:12:51 -0500

    Refactor Logo component to add responsive styling

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,5 +1,7 @@
 function Logo() {
-  return <img src='logo.svg' alt='Company Logo' />;
+  return (
+    <img src='logo.svg' alt='Company Logo' className='h-8 w-7 md:h-9 md:w-8' />
+  );
 }
 
 export default Logo;
