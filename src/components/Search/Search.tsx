11ab4942fea687237b6caae57097a22e24f34495
Usman Butt <usman.butt@outlook.com>
2024-01-07 23:12:04 -0500

    Remove unnecessary padding in Search component

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -37,7 +37,7 @@ function Search() {
   const result = data ? data[0] : null;
 
   return (
-    <search className='p-4 text-primary-foreground'>
+    <search className='text-primary-foreground'>
       <SearchInput
         inputWord={inputWord}
         setInputWord={setInputWord}
