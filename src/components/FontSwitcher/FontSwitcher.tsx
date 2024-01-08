b68d78c24e94c8788020bc06c556cbe3e57730a3
Usman Butt <usman.butt@outlook.com>
2024-01-08 08:47:30 -0500

    Refactor FontSwitcher component styling

--- src/components/FontSwitcher/FontSwitcher.tsx
+++ src/components/FontSwitcher/FontSwitcher.tsx
@@ -1,20 +1,20 @@
 function FontSwitcher() {
   return (
-    <div>
+    <>
       <label htmlFor='location' className='sr-only'>
         Location
       </label>
       <select
         id='location'
         name='location'
-        className='font-serif border-0 text-primary-foreground bg-primary'
+        className='font-serif border-0 text-primary-foreground bg-primary focus:ring-0 focus:outline-primary-accent'
         defaultValue='Sans Serif'
       >
         <option>Sans Serif</option>
         <option>Serif</option>
         <option>Mono</option>
       </select>
-    </div>
+    </>
   );
 }
 
