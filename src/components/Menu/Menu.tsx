6725c51d4d6d8e3e0ca4ddd92370dcdf42a57b84
Usman Butt <usman.butt@outlook.com>
2024-01-08 08:14:04 -0500

    Update Menu component styling

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -3,9 +3,10 @@ import ThemeSwitcher from '../ThemeSwitcher';
 
 function Menu() {
   return (
-    <menu className='flex items-center'>
-      <li>
+    <menu className='flex items-center gap-2'>
+      <li className='flex items-center'>
         <FontSwitcher />
+        <span className='after:content-["|"] text-[32px] text-secondary-accent ' />
       </li>
       <li>
         <ThemeSwitcher />
