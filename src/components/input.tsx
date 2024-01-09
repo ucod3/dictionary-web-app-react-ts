4756c95d7139466b1cf1084989a6e08feaa4e88b
Usman Butt <usman.butt@outlook.com>
2024-01-09 11:13:35 -0500

    Refactor input component styles

--- src/components/input.tsx
+++ src/components/input.tsx
@@ -32,19 +32,26 @@ const Input = forwardRef<
         'relative block w-full',
 
         // Background color + shadow applied to inset pseudo element, so shadow blends with border in light mode
-        // 'before:absolute before:inset-px before:rounded-[calc(theme(borderRadius.lg)-1px)] before:bg-white before:shadow',
+        'before:absolute before:inset-px before:rounded-[calc(theme(borderRadius.lg)-1px)] before:bg-white before:shadow',
 
         // Background color is moved to control and shadow is removed in dark mode so hide `before` pseudo
         'dark:before:hidden',
 
         // Focus ring
+        // 'after:pointer-events-none after:absolute after:inset-0 after:rounded-lg after:ring-inset after:ring-transparent sm:after:focus-within:ring-2sm:after:focus-within:ring-primary-accent',
         'after:pointer-events-none after:absolute after:inset-0 after:rounded-lg after:ring-inset after:ring-primary-accent sm:after:has-[[data-focus]]:ring-2 sm:after:has-[[data-focus]]:ring-primary-accent',
 
+        // Active
+        'active:ring-2 active:rounded-lg  active:ring-primary-accent',
+
+        // Hover
+        'hover:ring-2 hover:rounded-lg  hover:ring-primary-accent has-[[data-invalid]]:ring-2]]  has-[[data-invalid]]:hover:rounded-lg]] has-[[data-invalid]]:hover:ring-error]]',
+
         // Disabled state
-        'has-[[data-disabled]]:opacity-50 before:has-[[data-disabled]]:bg-zinc-950/5 before:has-[[data-disabled]]:shadow-none',
+        'has-[[data-disabled]]:opacity-50 before:has-[[data-disabled]]:bg-secondary-foreground before:has-[[data-disabled]]:shadow-none',
 
         // Invalid state
-        'before:has-[[data-invalid]]:shadow-red-500/10',
+        'before:has-[[data-invalid]]:shadow-error',
       ])}
     >
       <HeadlessInput
@@ -68,10 +75,12 @@ const Input = forwardRef<
             ],
 
           // Basic layout
-          'relative block w-full appearance-none rounded-xl px-[calc(theme(spacing[3.5])-1px)] py-[calc(theme(spacing[2.5])-1px)] md:py-[calc(theme(spacing[3.5])-1px)]  pl-6 ',
+          'relative block w-full appearance-none rounded-xl py-[calc(0.9375rem)] md:py-[calc(1.25rem)] pl-6',
 
           // Typography
-          'md:text-heading-s text-primary-foreground placeholder:text-primary-foreground placeholder:opacity-25 text-body-m font-bold ',
+
+          'text-base sm:text-lg text-primary-foreground font-bold placeholder:text-primary-foreground placeholder:opacity-25  ',
+          'focus:placeholder-opacity-50 focus:placeholder-primary-foreground',
 
           // Border
           'border-0',
@@ -83,10 +92,10 @@ const Input = forwardRef<
           'focus:outline-none ',
 
           // Invalid state
-          'data-[invalid]:border-error data-[invalid]:data-[hover]:error0 data-[invalid]',
+          'data-[invalid]:ring-2 data-[invalid]:ring-error ',
 
           // Disabled state
-          'data-[disabled]:border-zinc-950/20 dark:data-[hover]:data-[disabled]:border-white/15 data-[disabled]:dark:border-white/15 data-[disabled]:dark:bg-white/[2.5%]',
+          'data-[disabled]:border-secondary-foreground',
         ])}
         {...props}
       />
