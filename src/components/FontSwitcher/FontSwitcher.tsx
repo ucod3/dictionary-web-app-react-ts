b73d0adea2907a728e140870277e0eb48215f194
Usman Butt <usman.butt@outlook.com>
2024-01-08 22:22:39 -0500

    Refactor FontSwitcher component to use custom Select component

--- src/components/FontSwitcher/FontSwitcher.tsx
+++ src/components/FontSwitcher/FontSwitcher.tsx
@@ -1,19 +1,31 @@
+import Select from '../select';
+import useFont from '../../hooks/useFont';
+import { Font } from '../../types/fontTypes';
+
 function FontSwitcher() {
+  const { font, setFont } = useFont();
+
+  const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const newFont = e.target.value as Font;
+    setFont(newFont);
+  };
+
   return (
     <>
-      <label htmlFor='location' className='sr-only'>
-        Location
+      <label htmlFor='fontSwitcher' className='sr-only'>
+        Font Switcher
       </label>
-      <select
-        id='location'
-        name='location'
-        className='-mr-4 font-serif border-0 text-primary-foreground bg-primary focus:ring-0 focus:outline-primary-accent'
-        defaultValue='Sans Serif'
+      <Select
+        aria-label='Font Selection'
+        name='selected-font'
+        id='fontSwitcher'
+        value={font}
+        onChange={handleChange}
       >
-        <option>Sans Serif</option>
-        <option>Serif</option>
-        <option>Mono</option>
-      </select>
+        <option value='sans'>Sans Serif</option>
+        <option value='serif'>Serif</option>
+        <option value='mono'>Mono</option>
+      </Select>
     </>
   );
 }
