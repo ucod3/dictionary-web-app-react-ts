f5eaf5fe28df143c1783ede537302ccdb666890c
Usman Butt <usman.butt@outlook.com>
2024-01-08 22:23:24 -0500

    Add FontProvider component to main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import App from './components/App';
 import './index.css';
 import ThemeProvider from './components/ThemeProvider';
+import FontProvider from './components/FontProvider';
 
 ReactDOM.createRoot(document.querySelector('#root')!).render(
   <StrictMode>
     <ThemeProvider>
-      <App />
+      <FontProvider>
+        <App />
+      </FontProvider>
     </ThemeProvider>
   </StrictMode>,
 );
