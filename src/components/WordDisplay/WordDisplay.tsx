1e7012344ab8d0054c49afb1ae9f837305ff1be2
Usman Butt <usman.butt@outlook.com>
2024-01-10 10:05:37 -0500

    Add font styling and update heading sizes in WordDisplay component

--- src/components/WordDisplay/WordDisplay.tsx
+++ src/components/WordDisplay/WordDisplay.tsx
@@ -1,4 +1,5 @@
-import { useId, useState } from 'react';
+import { useEffect, useId, useState } from 'react';
+import useFont from '../../hooks/useFont';
 
 type Phonetic = {
   text: string;
@@ -40,6 +41,19 @@ function WordDisplay({ result }: WordDisplayProps) {
   }
   const [hover, setHover] = useState(false);
 
+  const [isSerif, setIsSerif] = useState(false);
+
+  const { font } = useFont();
+
+  useEffect(() => {
+    if (font === 'sans') {
+      setIsSerif(true);
+    }
+    if (font === 'serif' || font === 'mono') {
+      setIsSerif(false);
+    }
+  }, [font]);
+
   return (
     <article>
       <section
@@ -48,9 +62,9 @@ function WordDisplay({ result }: WordDisplayProps) {
         onMouseLeave={() => setHover(false)}
       >
         <div className='flex flex-col gap-2'>
-          <h2 className='text-xl font-bold md:text-2xl '>{result.word}</h2>
+          <h2 className='font-bold text-1xl md:text-2xl '>{result.word}</h2>
           {firstPhonetic && (
-            <p className='text-base md:text-xl text-primary-accent'>
+            <p className='text-md md:text-xl text-primary-accent'>
               {firstPhonetic.text}
             </p>
           )}
@@ -80,27 +94,32 @@ function WordDisplay({ result }: WordDisplayProps) {
         return (
           <section key={uniquePartOfSpeechId}>
             <div className='flex items-center mb-5 mt-7 md:mt-10 md:mb-6'>
-              <h3 className='text-xl font-bold'>{meaning.partOfSpeech}</h3>
+              <h3
+                className={`text-md md:text-xl font-bold ${
+                  isSerif ? 'italic' : 'normal'
+                }`}
+              >
+                {meaning.partOfSpeech}
+              </h3>
               <hr className='flex-grow ml-4 border-secondary-accent' />
             </div>
 
-            <h3 className='mb-2 font-bold text-l'>Meaning</h3>
+            <h3 className='mb-2 text-base text-secondary-foreground md:text-lg'>
+              Meaning
+            </h3>
             <ul
               key={uniqueMeaningId}
-              className={`mx-6 md:mx-9 mt-6 list-disc list-outside marker:text-primary-accent ${
+              className={`text-base-b md:text-md mx-6 md:mx-9 mt-6 list-disc list-outside marker:text-primary-accent ${
                 meaning.synonyms.length > 0 ? 'mb-6 md:mb-10 lg:mb-16' : ''
               }`}
             >
               {meaning.definitions.map((definition, definitionIndex) => {
                 const uniqueDefinitionId = `${uniqueMeaningId}-definition-${definitionIndex}`;
                 return (
-                  <li
-                    key={uniqueDefinitionId}
-                    className='mb-[13px] text-base-b md:text-md '
-                  >
+                  <li key={uniqueDefinitionId} className='mb-[13px] '>
                     {definition.definition}
                     {definition.example && (
-                      <blockquote className='text-base-b md:text-md  text-quote-foreground my-[13px] before:content-["“"] after:content-["”"]'>
+                      <blockquote className='text-quote-foreground my-[13px] before:content-["“"] after:content-["”"]'>
                         {definition.example}
                       </blockquote>
                     )}
@@ -110,13 +129,16 @@ function WordDisplay({ result }: WordDisplayProps) {
             </ul>
 
             {meaning.synonyms.length > 0 && (
-              <section className='mb-2'>
-                <h4 className='mb-2 text-lg font-bold'>Synonyms</h4>
+              <section className='flex gap-6 mb-2 text-base md:text-lg'>
+                <h4 className='mb-2 text-secondary-foreground'>Synonyms</h4>
                 <ul className='list-disc list-inside'>
                   {meaning.synonyms.slice(0, 3).map((synonym, synonymIndex) => {
                     const uniqueSynonymId = `${uniqueMeaningId}-synonym-${synonymIndex}`;
                     return (
-                      <li key={uniqueSynonymId} className='mb-1 list-none'>
+                      <li
+                        key={uniqueSynonymId}
+                        className='mb-1 font-bold list-none text-primary-accent'
+                      >
                         {synonym}
                       </li>
                     );
