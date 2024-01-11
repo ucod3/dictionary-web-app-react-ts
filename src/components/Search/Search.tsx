8992750ba832b220983225298f8370a6608215c8
Usman Butt <usman.butt@outlook.com>
2024-01-11 18:48:30 -0500

    Add useEffect hook to handle data retrieval

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import useSWR from 'swr';
 import SearchInput from '../SearchInput';
 import WordDisplay from '../WordDisplay';
@@ -8,30 +8,43 @@ const api = 'https://api.dictionaryapi.dev/api/v2/entries/en/';
 function Search() {
   const [inputWord, setInputWord] = useState('');
   const [searchWord, setSearchWord] = useState('');
-  const [wordNotFound, setWordNotFound] = useState(false);
   const [result, setResult] = useState(null);
 
   const fetcher = async (url: string) => {
     const response = await fetch(url);
+    console.log(response);
     if (!response.ok) {
-      throw new Error('No Definitions Found');
+      // throw new Error('No Definitions Found');
+      const error = await response.json();
+      console.log(error);
     }
 
     const data = await response.json();
+    console.log(data);
     return data[0];
   };
 
-  const { data, error } = useSWR(
+  const { data, error, isLoading } = useSWR(
     searchWord ? `${api}${searchWord}` : null,
     fetcher,
-    {
-      onError: () => setWordNotFound(true),
-      onSuccess: (apiData) => {
-        setResult(apiData);
-        setWordNotFound(false);
-      },
-    },
+    // {
+    //   onError: () => setWordNotFound(true),
+    //   onSuccess: (apiData) => {
+    //     console.log(apiData);
+    //     console.log(error);
+    //     setResult(apiData);
+    //     setWordNotFound(!apiData || apiData.length === 0);
+    //   },
+    // },
   );
+
+  useEffect(() => {
+    if (data) {
+      setResult(data);
+      // setWordNotFound(false);
+      // setWordNotFound(!data || data.length === 0);
+    }
+  }, [data]);
 
   const handleSubmit = useCallback(
     (e: React.FormEvent<HTMLFormElement>) => {
@@ -46,8 +59,6 @@ function Search() {
     setSearchWord(word); // perform the search with the new word
   };
 
-  if (!data && searchWord && !wordNotFound) return <div>Loading...</div>;
-
   return (
     <search className='text-primary-foreground'>
       <SearchInput
@@ -55,12 +66,20 @@ function Search() {
         setInputWord={setInputWord}
         handleSubmit={handleSubmit}
       />
-      {result && (
-        <WordDisplay
-          result={result || null}
-          setSearchWord={updateSearchWord}
-          wordNotFound={wordNotFound}
-        />
+      {error ? (
+        <article className='flex flex-col my-8 text-center  md:my-28 w-full max-w-[736px] mx-auto'>
+          <h2 className='my-6 font-bold text-md md:text-lg text-primary-foreground'>
+            No Definitions Found
+          </h2>
+          <p className='order-last text-base md:text-md text-secondary-foreground '>
+            Sorry pal, we couldn&#39;t find definitions for the word you were
+            looking for. You can try the search again at later time or head to
+            the web instead.
+          </p>
+          <p className='order-first text-2xl'>😕</p>
+        </article>
+      ) : (
+        <WordDisplay result={result || null} setSearchWord={updateSearchWord} />
       )}
     </search>
   );
