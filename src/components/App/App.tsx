f40922e4ef84e80197ca76c48edaa639f096993a
Usman Butt <usman.butt@outlook.com>
2024-01-15 21:09:14 -0500

    Update container padding in App component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -3,7 +3,7 @@ import Search from '../Search';
 
 function App() {
   return (
-    <section className='min-h-screen overflow-hidden bg-primary container px-6 pt-6 mx-auto md:px-10 md:pt-14 lg:max-2xl:max-w-[736px] xl:container'>
+    <section className='overflow-hidden min-h-dvh container p-6 mx-auto md:px-10 md:pt-14 lg:max-2xl:max-w-[736px] xl:container'>
       <Header />
       <Search />
     </section>
