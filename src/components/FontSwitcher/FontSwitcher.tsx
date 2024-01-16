62224d94eb3879f6e5d4c8b419c11155aa40f2c1
Usman Butt <usman.butt@outlook.com>
2024-01-15 21:19:17 -0500

    Refactor FontSwitcher component to use Dropdown component

--- src/components/FontSwitcher/FontSwitcher.tsx
+++ src/components/FontSwitcher/FontSwitcher.tsx
@@ -1,32 +1,64 @@
-import Select from '../select';
 import useFont from '../../hooks/useFont';
 import { Font } from '../../types/fontTypes';
+import {
+  Dropdown,
+  DropdownButton,
+  DropdownItem,
+  DropdownMenu,
+} from '../dropdown';
 
 function FontSwitcher() {
   const { font, setFont } = useFont();
 
-  const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    const newFont = e.target.value as Font;
+  const handleFontChange = (newFont: Font) => {
     setFont(newFont);
   };
 
   return (
-    <>
-      <label htmlFor='fontSwitcher' className='sr-only'>
-        Font Switcher
-      </label>
-      <Select
-        aria-label='Font Selection'
-        name='selected-font'
-        id='fontSwitcher'
-        value={font}
-        onChange={handleChange}
-      >
-        <option value='sans'>Sans Serif</option>
-        <option value='serif'>Serif</option>
-        <option value='mono'>Mono</option>
-      </Select>
-    </>
+    <Dropdown>
+      <DropdownButton>
+        {
+          {
+            sans: 'Sans Serif',
+            serif: 'Serif',
+            mono: 'Mono',
+          }[font as keyof { sans: string; serif: string; mono: string }]
+        }
+        <svg
+          xmlns='http://www.w3.org/2000/svg'
+          width='14'
+          height='8'
+          viewBox='0 0 14 8'
+        >
+          <path
+            fill='none'
+            stroke='#A445ED'
+            strokeWidth='1.5'
+            d='m1 1 6 6 6-6'
+          />
+        </svg>
+      </DropdownButton>
+      <DropdownMenu>
+        <DropdownItem
+          onClick={() => handleFontChange('sans')}
+          className='font-sans text-primary-accent'
+        >
+          Sans Serif
+        </DropdownItem>
+        <DropdownItem
+          onClick={() => handleFontChange('serif')}
+          className='font-serif'
+        >
+          Serif
+        </DropdownItem>
+        <DropdownItem
+          onClick={() => handleFontChange('mono')}
+          className='font-mono'
+        >
+          Mono
+        </DropdownItem>
+      </DropdownMenu>
+    </Dropdown>
   );
 }
 
