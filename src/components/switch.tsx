e096126b7ba230148e75160aec717e34ad89d06c
Usman Butt <usman.butt@outlook.com>
2024-01-17 18:33:04 -0500

    Refactor switch components and styles

--- src/components/switch.tsx
+++ src/components/switch.tsx
@@ -1,59 +1,9 @@
 import {
-  Field as HeadlessField,
   Switch as HeadlessSwitch,
-  type FieldProps as HeadlessFieldProps,
   type SwitchProps as HeadlessSwitchProps,
 } from '@headlessui/react';
 import { clsx } from 'clsx';
 import type React from 'react';
-
-export function SwitchGroup({
-  className,
-  ...props
-}: React.ComponentPropsWithoutRef<'div'>) {
-  return (
-    <div
-      data-slot='control'
-      {...props}
-      className={clsx(
-        className,
-
-        // Basic groups
-        'space-y-3 [&_[data-slot=label]]:font-normal',
-
-        // With descriptions
-        'has-[[data-slot=description]]:space-y-6 [&_[data-slot=label]]:has-[[data-slot=description]]:font-medium',
-      )}
-    />
-  );
-}
-
-export function SwitchField({ className, ...props }: HeadlessFieldProps) {
-  return (
-    <HeadlessField
-      data-slot='field'
-      {...props}
-      className={clsx(
-        className,
-
-        // Base layout
-        'grid grid-cols-[1fr_auto] items-center gap-x-8 gap-y-1 sm:grid-cols-[1fr_auto]',
-
-        // Control layout
-        '[&>[data-slot=control]]:col-start-2 [&>[data-slot=control]]:self-center',
-
-        // Label layout
-        '[&>[data-slot=label]]:col-start-1 [&>[data-slot=label]]:row-start-1 [&>[data-slot=label]]:justify-self-start',
-
-        // Description layout
-        '[&>[data-slot=description]]:col-start-1 [&>[data-slot=description]]:row-start-2',
-
-        // With description
-        '[&_[data-slot=label]]:has-[[data-slot=description]]:font-medium',
-      )}
-    />
-  );
-}
 
 const colors = {
   'toggle-accent': [
@@ -64,7 +14,7 @@ const colors = {
 
 type Color = keyof typeof colors;
 
-export function Switch({
+export default function Switch({
   color = 'toggle-accent',
   className,
   children,
@@ -90,14 +40,16 @@ export function Switch({
         'forced-colors:outline forced-colors:[--switch-bg:Highlight] ]',
 
         // Unchecked
-        'bg-[--switch-bg] :ring-[--switch-bg-ring]]',
+        'bg-[--switch-bg] ring-[--switch-bg-ring]',
 
         // Checked
         'data-[checked]:bg-[--switch-bg] data-[checked]:ring-[--switch-bg-ring]',
 
         // Focus
-        // 'focus:outline-none',
-        'focus:outline-none data-[focus]:outline data-[focus]:outline-2 data-[focus]:outline-offset-2 data-[focus]:outline-primary-accent',
+        'data-[focus]:outline data-[focus]:outline-2 data-[focus]:outline-offset-2 data-[focus]:outline-primary-accent cursor-pointer',
+
+        // Focus Visible
+        'focus-visible:outline  focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary-accent',
 
         // Color specific styles
         colors[color as Color],
