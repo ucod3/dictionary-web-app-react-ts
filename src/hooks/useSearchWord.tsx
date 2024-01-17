a21a07c49731519ecb653187973adb2c66a6ba7c
Usman Butt <usman.butt@outlook.com>
2024-01-16 21:10:26 -0500

    Remove console.log statements from useSearchWord hook

--- src/hooks/useSearchWord.tsx
+++ src/hooks/useSearchWord.tsx
@@ -9,13 +9,11 @@ function useSearchWord({ searchWord, setWordNotFound }: UseSearchWordProps) {
   const api = 'https://api.dictionaryapi.dev/api/v2/entries/en/';
   const fetcher = async (url: string) => {
     const response = await fetch(url);
-    console.log(response);
     if (!response.ok) {
       throw new Error('No Definitions Found');
     }
 
     const data = await response.json();
-    console.log(data);
     return data[0];
   };
 
