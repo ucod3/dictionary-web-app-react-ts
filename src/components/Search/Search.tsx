03a69941a6845fe8f451b59e6f9fed7a666c4e58
Usman Butt <usman.butt@outlook.com>
2024-01-16 20:37:30 -0500

    Refactor search functionality and handle loading and not found states

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,60 +1,59 @@
 import { useCallback, useEffect, useState } from 'react';
-import useSWR from 'swr';
 import SearchInput from '../SearchInput';
 import WordDisplay from '../WordDisplay';
-
-const api = 'https://api.dictionaryapi.dev/api/v2/entries/en/';
+import NotFound from '../NotFound';
+import useSearchWord from '../../hooks/useSearchWord';
 
 function Search() {
   const [inputWord, setInputWord] = useState('');
   const [searchWord, setSearchWord] = useState('');
   const [result, setResult] = useState(null);
   const [isSubmitted, setIsSubmitted] = useState(false);
+  const [wordNotFound, setWordNotFound] = useState(false);
 
-  const fetcher = async (url: string) => {
-    const response = await fetch(url);
-    if (!response.ok) {
-      throw new Error('No Definitions Found');
-    }
-
-    const data = await response.json();
-    return data[0];
-  };
-
-  const { data, error, isLoading } = useSWR(
-    searchWord ? `${api}${searchWord}` : null,
-    fetcher,
-    // {
-    //   onError: () => setWordNotFound(true),
-    //   onSuccess: (apiData) => {
-    //     console.log(apiData);
-    //     console.log(error);
-    //     setResult(apiData);
-    //     setWordNotFound(!apiData || apiData.length === 0);
-    //   },
-    // },
-  );
+  const { data, isLoading } = useSearchWord({ searchWord, setWordNotFound });
 
   useEffect(() => {
     if (data) {
       setResult(data);
-      // setWordNotFound(false);
-      // setWordNotFound(!data || data.length === 0);
+    } else {
+      setResult(null);
     }
   }, [data]);
 
   const handleSubmit = useCallback(
     (e: React.FormEvent<HTMLFormElement>) => {
       e.preventDefault();
-      setSearchWord(inputWord);
+      const value = inputWord.trim();
+      if (value !== '') {
+        setSearchWord(inputWord);
+      }
     },
     [inputWord],
   );
 
   const updateSearchWord = (word: string) => {
     setInputWord(word); // update the input field with the new word
-    setSearchWord(word); // perform the search with the new word
+    if (word.trim() !== '') {
+      setSearchWord(word); // perform the search with the new word
+    }
   };
+
+  let content = null;
+
+  if (isLoading) {
+    content = <p>Loading...</p>;
+  }
+
+  if (wordNotFound) {
+    content = <NotFound wordNotFound={wordNotFound} />;
+  }
+
+  if (result) {
+    content = (
+      <WordDisplay result={result || null} setSearchWord={updateSearchWord} />
+    );
+  }
 
   return (
     <search className='text-primary-foreground'>
@@ -65,26 +64,9 @@ function Search() {
         setResult={setResult}
         isSubmitted={isSubmitted}
         setIsSubmitted={setIsSubmitted}
+        setWordNotFound={setWordNotFound}
       />
-      {error ? (
-        <article className='flex flex-col my-8 text-center  md:my-28 w-full max-w-[736px] mx-auto'>
-          <h2 className='my-6 font-bold text-md md:text-lg text-primary-foreground'>
-            No Definitions Found
-          </h2>
-          <p className='order-last text-base md:text-md text-secondary-foreground '>
-            Sorry pal, we couldn&#39;t find definitions for the word you were
-            looking for. You can try the search again at later time or head to
-            the web instead.
-          </p>
-          <p className='order-first text-2xl'>😕</p>
-        </article>
-      ) : (
-        <WordDisplay
-          result={result || null}
-          setSearchWord={updateSearchWord}
-          isSubmitted={isSubmitted}
-        />
-      )}
+      {content}
     </search>
   );
 }
