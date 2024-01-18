0a2f8da20f1e46bbfdf53e9418731c1dbc69af91
Usman Butt <usman.butt@outlook.com>
2024-01-18 08:01:24 -0500

    Update Footer component

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,7 +1,27 @@
-type FooterProps = {};
-
-function Footer(props: FooterProps) {
-  return null;
+function Footer() {
+  return (
+    <footer className='text-center text-[11px] text-secondary-foreground'>
+      <p>
+        Challenge by{' '}
+        <a
+          rel='noopener noreferrer'
+          href='https://www.frontendmentor.io/challenges/dictionary-web-app-h5wwnyuKFL'
+          target='_blank'
+        >
+          Frontend Mentor
+        </a>
+        . Coded by{' '}
+        <a
+          rel='noopener noreferrer'
+          href='https://github.com/ucod3'
+          target='_blank'
+        >
+          Usman Butt
+        </a>
+        .
+      </p>
+    </footer>
+  );
 }
 
 export default Footer;
